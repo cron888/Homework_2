350fc43b58bb02de50a2e7b5cffddccd07945fab
Andrey <ancient684@gmail.com>
2026-02-21 21:31:02 +0300

    Задача 7 - остаток от деления

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    var boxer1 = 78.2;
-    var boxer2 = 82.7;
+    var w1 = 7;
+    var w2 = 5;
 
-    System.out.println("Сумма= " + (boxer1 + boxer2));
-    System.out.println("Разница = "+ (boxer2 - boxer1));
+    System.out.println(w1 % w2);
 
 
 
