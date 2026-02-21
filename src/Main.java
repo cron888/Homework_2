2cd1f25a196b753ba4f401561de4e4b45dd0c440
Andrey <ancient684@gmail.com>
2026-02-21 21:20:23 +0300

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    var frog = 3.5;
+    var boxer1 = 78.2;
+    var boxer2 = 82.7;
 
-    System.out.println(frog);
-    frog = frog * 10;
-    System.out.println(frog);
-    frog = frog / 3.5;
-    System.out.println(frog);
-    frog = frog + 4;
-    System.out.println(frog);
+    System.out.println("Сумма= " + (boxer1 + boxer2));
+    System.out.println("Разница = "+ (boxer2 - boxer1));
+
 
 
 }
