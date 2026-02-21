cdbd1a66be0a05734f9e30d08bbbc1793edbf9a8
Andrey <ancient684@gmail.com>
2026-02-21 20:44:29 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    var dog = 8.0 - 3.5;
-    var cat = 3.6 - 1.6;
-    var paper = 763789 - 7639;
+  var friend = 19;
 
     System.out.println(friend);
     friend = friend + 2;
